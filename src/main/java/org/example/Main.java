6e9d93334012976f76e2f3242bae348b6e65a686
pupil <pupil@ivkhk.ee>
2024-09-23 09:04:30 +0300

    Task1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package Jptv23;
+
+import java.util.Scanner;
+
+public class  Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in) ;
+        System.out.println("Java Hello");
+    }
+
+}
